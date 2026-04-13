ced5c350194a240d899914eb7a894335f3f8d9df
calebchamblee <chambleecaleb@gmail.com>
2026-04-13 00:30:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import time
 
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 datasets = ["blobs", "circles", "lines", "moons", "uniform"]
@@ -15,33 +15,87 @@ def index():
 
 
 # example POST request handle
-@app.route("/your_route", methods=["POST"])
-def your_route_func():
+@app.route("/init", methods=["POST"])
+def init():
     # parse request sent from the front end
-    request_data = request.get_json()
-    print("request_data", request_data)
+    data = request.json['data']
+    n_clusters = request.json['n_clusters']
+    # convert to np array with list comprehension
+    all_data = np.array([[point['x'], point['y']] for point in data])
+    centroids = []
+    # randomize centroids as a random point within the range of the data, for x and y
+    for i in range(n_clusters):
+        x = np.random.uniform(all_data[:, 0].min(), all_data[:, 0].max())
+        y = np.random.uniform(all_data[:, 1].min(), all_data[:, 1].max())
+        centroids.append([x, y])
+    # initialize labels to closest centroid to each point
+    labels = []
+    for i in range(len(data)):
+        distances = [((all_data[i][0] - centroid[0])**2 + (all_data[i][1] - centroid[1])**2)**0.5 for centroid in centroids]
+        # closest centroid to point (argmin gets index)
+        labels.append(np.argmin(distances))
 
-    # return data to front end
-    return dict(msg="a", data={"a": 1, "b": 2})
+    # reset history and initialize to only have the randomized centroids and the labels
+    history.clear()
+    history.append([centroids, labels])
 
-def step(data, centroids, labels):
-    new_labels = []
-    for point in data:
-        distances = [((point[0] - centroid[0])**2 + (point[1] - centroid[1])**2)**0.5 for centroid in centroids]
-        new_labels.append(np.argmin(distances))
+    # return centroids and labels to frontend to plot
+    return jsonify({'centroids': centroids, 'labels': labels.tolist()})
 
+@app.route("/step_forward", methods=["POST"])
+def step_forward():
+    # get data from frontend
+    data = request.json['data']
+    all_data = np.array([[point['x'], point['y']] for point in data])
+    # get centroids as last centroids in history
+    converged = step(all_data, history[-1][0])
+
+    # return centroids and labels to frontend to plot via history
+    return jsonify({'centroids': history[-1][0], 'labels': history[-1][1].tolist(), 'converged': converged})
+
+@app.route("/step_backward", methods=["POST"])
+def step_backward():
+    # step back...
+    stepBack()
+    # return centroids and labels to frontend to plot via history
+    return jsonify({'centroids': history[-1][0], 'labels': history[-1][1].tolist()})
+
+def step(data, centroids):
+    labels = []
+    # for each point, get distances from each centroid, choose minimum
+    for i in range(len(data)):
+        distances = [((data[i][0] - centroid[0])**2 + (data[i][1] - centroid[1])**2)**0.5 for centroid in centroids]
+        # closest centroid to point (argmin gets index)
+        labels.append(np.argmin(distances))
+
+    # compute new centroids
     new_centroids = []
+    labels = np.array(labels)
     for k in range(len(centroids)):
-        assigned data[new_labels == k]
-        if len(assigned > 0):
-            new_centroids.append(assigned.mean(axis=0))
-        else:
+        # get all points assigned to current centroid via boolean list
+        assigned = data[labels == k]
+        # if no points in centroid, don't move centroid
+        if len(assigned) == 0:
             new_centroids.append(centroids[k])
+        # create new centroid as mean of all points in centroid (mean(x), mean(y))
+        else:
+            mean_x = sum(point[0] for point in assigned) / len(assigned)
+            mean_y = sum(point[1] for point in assigned) / len(assigned)
+            new_centroids.append([mean_x, mean_y])
     
-    history.append([new_centroids, new_labels])
+    # check if last step didn't change centroids
+    converged = (new_centroids == history[-1][0])
+
+    # add centroids to history
+    history.append([new_centroids, labels])
+
+    # return if the algorithm has converged so we can determine out whether to plot
+    return converged
 
 def stepBack():
+    # only step back if possible
     if len(history) > 1:
+        # remove last entry of history and reassign centroids and labels to end of history
         history.pop()
         centroids = history[-1][0]
         labels = history[-1][1]
--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let svg = d3
   .attr("height", 500)
   .style("background", "#eee");
 
-load_and_plot("blobs.csv");
-example_use_of_post();
+init("blobs", 10);
 
+// Change to color clusters and account for n_clusters
 function load_and_plot(filename) {
-  d3.csv(`static/datasets/${filename}`, d3.autoType).then((data) => {
+  d3.csv(`static/datasets/${filename}.csv`, d3.autoType).then((data) => {
     console.log("data", data);
     svg
       .selectAll("circle")
@@ -21,6 +21,78 @@ function load_and_plot(filename) {
       .attr("cx", (d) => d.x * 20 + 50)
       .attr("cy", (d) => d.y * 20 + 250);
   });
+}
+
+// initialize the plot for given daset and num clusters
+async function init(filename, n_clusters) {
+  // read in data and convert to numbers
+  d3.csv(`static/datasets/${filename}.csv`, d3.autoType).then((data) => {
+      // then get data from back end, then plot
+      post('/init', {data: data, n_clusters: n_clusters}).then((result) => {
+        // add new plotting function here to plot clusters in different colors
+        // format: result.centroids: list of centroid points, result.labels: list of label for each point in data
+      });
+    });
+}
+
+// perform k-means clustering: take a step forward until converged
+async function run() {
+  // get name of dataset and load file
+  const filename = document.getElementById('dataset').value;
+  d3.csv(`static/datasets/${filename}.csv`, d3.autoType).then(async (data) => {
+    let converged = false;
+    // redraw for each step until converged, increment step # label
+    while (!converged) {
+      // get results of one step forward from backend
+      const result = await post('/step_forward', {data: data});
+      converged = result.converged;
+      if (!converged) {
+        document.getElementById('step').innerHTML = +(document.getElementById('step').innerHTML) + 1;
+      }
+      // plot here (same function as init)
+    }
+  });
+}
+
+// take on 'step' forward in k-means
+async function forward() {
+    const filename = document.getElementById('dataset').value;
+    // get name of dataset and load file
+    d3.csv(`static/datasets/${filename}.csv`, d3.autoType).then(async (data) => {
+      // get results of step forward from backend
+      const result = await post('/step_forward', {data: data});
+      if (!result.converged) {
+        // increment step # label unless converged
+        document.getElementById('step').innerHTML = +(document.getElementById('step').innerHTML) + 1;
+        // plot here (same function as init)
+      }
+  });
+}
+
+// take on 'step' backward in k-means
+async function backward() {
+    const filename = document.getElementById('dataset').value;
+    // get name of dataset and load file
+    d3.csv(`static/datasets/${filename}.csv`, d3.autoType).then(async (data) => {
+      // get results of step back from backend
+      const result = await post('/step_backward', {data: data});
+      if (!result.converged) {
+        // decrement step # label unless converged
+        document.getElementById('step').innerHTML = +(document.getElementById('step').innerHTML) - 1;
+        // plot here (same function as init)
+      }
+  });
+}
+
+// reset k-means clustering to be random again
+async function reset() {
+    // get current dataset and n_clusters
+    const filename = document.getElementById('dataset').value;
+    const n_clusters = document.getElementById('n_clusters').value;
+    // reset n_clusters label to 0
+    document.getElementById('step').innerHTML = 0;
+    // redraw plot from scratch
+    init(filename, n_clusters);
 }
 
 // utilities
@@ -36,10 +108,4 @@ async function post(url = "", data = {}) {
     throw new Error(`HTTP error! status: ${response.status}`); // Check for errors
   }
   return await response.json(); // Parse the JSON response
-}
-
-function example_use_of_post() {
-  post("your_route", {data: "aabbccc"}).then((returned_data) => {
-    console.log("server says", returned_data);
-  });
 }
